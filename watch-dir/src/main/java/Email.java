e6ccfc3e8c218d0e4fe5875a25d6fea9c71d0e49
sergey.bespalko <bespalko@live.ru>
2018-06-30 00:34:33 +0300

    reformat

--- watch-dir/src/main/java/Email.java
+++ watch-dir/src/main/java/Email.java
@@ -29,12 +29,11 @@
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
+import java.io.IOException;
 import java.nio.file.*;
-import static java.nio.file.StandardWatchEventKinds.*;
-import static java.nio.file.LinkOption.*;
-import java.nio.file.attribute.*;
-import java.io.*;
-import java.util.*;
+
+import static java.nio.file.StandardWatchEventKinds.ENTRY_CREATE;
+import static java.nio.file.StandardWatchEventKinds.OVERFLOW;
 
 /**
  * Example monitors a specified directory for new files.
@@ -46,82 +45,84 @@ import java.util.*;
 
 public class Email {
 
-    private final WatchService watcher;
-    private final Path dir;
+  private final WatchService watcher;
+  private final Path dir;
 
-    /**
-     * Creates a WatchService and registers the given directory
-     */
-    private Email(Path dir) throws IOException {
-        this.watcher = FileSystems.getDefault().newWatchService();
-        dir.register(watcher, ENTRY_CREATE);
-        this.dir = dir;
-    }
+  /**
+   * Creates a WatchService and registers the given directory
+   */
+  private Email(Path dir) throws IOException {
+    this.watcher = FileSystems.getDefault().newWatchService();
+    dir.register(watcher, ENTRY_CREATE);
+    this.dir = dir;
+  }
 
-    /**
-     * Process all events for the key queued to the watcher.
-     */
-    private void processEvents() {
-        for (;;) {
+  /**
+   * Process all events for the key queued to the watcher.
+   */
+  private void processEvents() {
+    for (; ; ) {
 
-            // wait for key to be signaled
-            WatchKey key;
-            try {
-                key = watcher.take();
-            } catch (InterruptedException x) {
-                return;
-            }
+      // wait for key to be signaled
+      WatchKey key;
+      try {
+        key = watcher.take();
+      } catch (InterruptedException x) {
+        return;
+      }
 
-            for (WatchEvent<?> event: key.pollEvents()) {
-                WatchEvent.Kind kind = event.kind();
+      for (WatchEvent<?> event : key.pollEvents()) {
+        WatchEvent.Kind kind = event.kind();
 
-                if (kind == OVERFLOW) {
-                    continue;
-                }
-
-                //The filename is the context of the event.
-                WatchEvent<Path> ev = (WatchEvent<Path>)event;
-                Path filename = ev.context();
-
-                //Verify that the new file is a text file.
-                try {
-                    Path child = dir.resolve(filename);
-                    if (!Files.probeContentType(child).equals("text/plain")) {
-                        System.err.format("New file '%s' is not a plain text file.%n", filename);
-                        continue;
-                    }
-                } catch (IOException x) {
-                    System.err.println(x);
-                    continue;
-                }
-
-                //Email the file to the specified email alias.
-                System.out.format("Emailing file %s%n", filename);
-                //Details left to reader....
-            }
-
-            //Reset the key -- this step is critical if you want to receive
-            //further watch events. If the key is no longer valid, the directory
-            //is inaccessible so exit the loop.
-            boolean valid = key.reset();
-            if (!valid) {
-                    break;
-            }
+        if (kind == OVERFLOW) {
+          System.out.println("kind == OVERFLOW");
+          continue;
         }
+
+        //The filename is the context of the event.
+        WatchEvent<Path> ev = (WatchEvent<Path>) event;
+        Path filename = ev.context();
+
+        //Verify that the new file is a text file.
+        try {
+          Path child = dir.resolve(filename);
+          if (!Files.probeContentType(child).equals("text/plain")) {
+            System.err.format("New file '%s' is not a plain text file.%n", filename);
+            continue;
+          }
+        } catch (IOException x) {
+          System.err.println(x);
+          continue;
+        }
+
+        //Email the file to the specified email alias.
+        System.out.format("Emailing file %s%n", filename);
+        //Details left to reader....
+      }
+
+      //Reset the key -- this step is critical if you want to receive
+      //further watch events. If the key is no longer valid, the directory
+      //is inaccessible so exit the loop.
+      boolean valid = key.reset();
+      if (!valid) {
+        break;
+      }
+    }
+  }
+
+  private static void usage() {
+    System.err.println("usage: java Email dir");
+    System.exit(-1);
+  }
+
+  public static void main(String[] args) throws IOException {
+    //parse arguments
+    if (args.length < 1) {
+      usage();
     }
 
-    private static void usage() {
-        System.err.println("usage: java Email dir");
-        System.exit(-1);
-    }
-
-    public static void main(String[] args) throws IOException {
-        //parse arguments
-        if (args.length < 1)
-            usage();
-
-        //register directory and process its events
-        Path dir = Paths.get(args[0]);
-        new Email(dir).processEvents();
-    }
+    //register directory and process its events
+    Path dir = Paths.get(args[0]);
+    new Email(dir).processEvents();
+  }
 }
